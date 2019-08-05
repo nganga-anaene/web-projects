ed85f5b0f1cb8de649d75130c9016745175c57f2
Nganga Anaene <ngangaanaene@gmail.com>
2019-08-05 11:46:12 +0100

    base server

---
+++ airline/airline-server/src/main/java/com/anaene/airlineserver/AirlineServerApplication.java
@@ -0,0 +1,13 @@
+package com.anaene.airlineserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirlineServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AirlineServerApplication.class, args);
+    }
+
+}
